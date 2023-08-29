473c67c7977cda097b349723f8aa686d9dbcdcba
Agungfir08 <agung.firmansyah@ui.ac.id>
2023-08-29 22:45:29 +0700

    add task component but not done yet

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Login from "./page/Login";
 import Register from "./page/Register";
 import Sidebar from "./component/Sidebar";
+import Task from "./component/Task";
 
 export default function App() {
   return (
     // <Login />
     // <Register />
-    <Sidebar />
+    <Task />
   );
 }
