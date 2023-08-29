4659bb08ab2569c255e39c94c4b19cfa7a3e02e3
Agungfir08 <agung.firmansyah@ui.ac.id>
2023-08-29 15:35:54 +0700

    add sidebar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Login from "./page/Login";
 import Register from "./page/Register";
+import Sidebar from "./component/Sidebar";
 
 export default function App() {
   return (
     // <Login />
-    <Register />
+    // <Register />
+    <Sidebar />
   );
 }
