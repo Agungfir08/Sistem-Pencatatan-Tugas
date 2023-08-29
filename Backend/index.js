d1c4e4a98d517bd434580a10af3804dbef411535
IbnuHs <ibnuhs30@gmail.com>
2023-08-29 08:30:36 +0800

    add nodemin and port BackEnd

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,8 @@
-import Express  from 'express';
+import Express from "express";
 
 const app = Express();
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`your App Running in port : ${port}`);
+});
