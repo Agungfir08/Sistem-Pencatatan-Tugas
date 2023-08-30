75df06a585836c10291624490f0831b282a2a71e
arielcatur <arielcatur1305@gmail.com>
2023-08-30 10:31:55 +0800

    update home and modal

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import React from "react";
-import Login from "./page/Login";
-import Register from "./page/Register";
+// import React from "react";
+// import Login from "./page/Login";
+import Home from "./page/Home";
+import Sidebar from "./components/Sidebar";
+// import Modal from "./page/Home2";
+// import Register from "./page/Register";
 
 export default function App() {
   return (
-    // <Login />
-    <Register />
+    <>
+      {/* <Login /> */}
+        {/* <Register /> */}
+      {/* <Profile /> */}
+      <Sidebar />
+      <Home />
+      {/* <Modal /> */}
+    </>
   );
+    
 }
