030b1cb3c6d545bfefe5fdf5117ffc532af562fa
IbnuHs <ibnuhs30@gmail.com>
2023-09-01 16:04:12 +0800

    edit verfiy token

--- Backend/src/middleware/verifyToken.js
+++ Backend/src/middleware/verifyToken.js
@@ -4,12 +4,12 @@ const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
   // const token = window.localStorage.getItem('token')
 
-  if (!token) res.status(401).json({ message: "Token is Empty" });
+  if (!token) return res.status(401).json({ message: "Token is Empty" });
 
   jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
     console.log(err);
     if (err) return res.status(403).json({ message: err });
-    req.id = decoded.id
+    req.id = decoded.id;
     console.log("Verifikasi Token Berhasil");
     next();
   });
