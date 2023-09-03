af60439859c855618d83afc6e7c82b434bfbb979
Agungfir08 <agung.firmansyah@ui.ac.id>
2023-09-03 21:06:50 +0700

    add empty component

--- Frontend/src/page/Login.jsx
+++ Frontend/src/page/Login.jsx
@@ -56,6 +56,7 @@ export default function Login() {
           console.log(err.res.data.message);
         } else {
           alert(err.message);
+          console.log(err.message);
         }
       });
   }
--- Frontend/src/page/Register.jsx
+++ Frontend/src/page/Register.jsx
@@ -64,6 +64,7 @@ export default function Register() {
           console.log(err.res.data.message);
         } else {
           alert(err.message);
+          console.log(err.message);
         }
       });
   }
