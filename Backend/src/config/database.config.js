7879ab7074adfbffe19cdd534165ff167317dbb2
IbnuHs <ibnuhs30@gmail.com>
2023-09-05 15:17:24 +0800

    e

--- Backend/src/config/database.config.js
+++ Backend/src/config/database.config.js
@@ -1,17 +1,17 @@
 import Sequelize from "sequelize";
 import mysql2 from "mysql2";
 
-// const database = new Sequelize("sql12643089", "sql12643089", "gtwB8A5jCX", {
-//   host: "sql12.freesqldatabase.com",
-//   dialect: "mysql",
-//   dialectModule: mysql2,
-// });
-
-const database = new Sequelize("task_list", "root", "", {
-  host: "localhost",
+const database = new Sequelize("sql12643089", "sql12643089", "gtwB8A5jCX", {
+  host: "sql12.freesqldatabase.com",
   dialect: "mysql",
   dialectModule: mysql2,
 });
+
+// const database = new Sequelize("task_list", "root", "", {
+//   host: "localhost",
+//   dialect: "mysql",
+//   dialectModule: mysql2,
+// });
 
 try {
   database.authenticate();
